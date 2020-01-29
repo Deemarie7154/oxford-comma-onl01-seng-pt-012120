23f662bcc5b7b649b21d945fd91f7fcb28cf605f
Dwight Marie <dwightmarie@gmail.com>
2020-01-29 06:04:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+ 
+case 
+when array.length == 1
+  array.join('')
+when array.length == 2
+  array.insert(1, " and ").join()
+when array.length == 3
+  array.insert(2, " and ").join()
+when array.length > 3
+  "The tank is almost full."
+else
+  "Error: capacity has an invalid value (#{capacity})"
 end
+end
+
+
