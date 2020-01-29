5e6f7ff339f4d5deaca7a75f4d20e24420f434e4
Dwight Marie <dwightmarie@gmail.com>
2020-01-29 06:25:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,14 @@ when array.length == 1
 when array.length == 2
   array.insert(1, " and ").join()
 when array.length == 3
-  
+     a.each do|items| 
+    if items == a[2]
+    a.pop
+    a << "and starfruit"
+   b = puts a.join(", ")
+    end
+    
+    end
 when array.length > 3
   "The tank is almost full."
 else
